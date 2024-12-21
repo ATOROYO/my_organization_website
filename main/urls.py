e4863010a48c71505e414ce415975a3be8e24a18
ATOROYO <davidatoroyosika@gmail.com>
2024-12-21 23:22:58 +0300

    Wrote code for urls.py

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('team/', views.team, name='team'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contact/', views.contact, name='contact'),
+    path('search/', views.search_results, name='search_results'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+]
