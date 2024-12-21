e157e1dcf795306841b99289679bbc5b9ff9607b
ATOROYO <davidatoroyosika@gmail.com>
2024-12-21 23:47:02 +0300

    Wrote JavScript code for the toggle

---
+++ main/static/js/scripts.js
@@ -0,0 +1,6 @@
+const toggleNavButton = document.querySelector(".toggle-nav");
+const navLinks = document.querySelector(".nav-links");
+
+toggleNavButton.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
