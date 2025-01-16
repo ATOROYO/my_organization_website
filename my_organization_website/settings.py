bdef79071ca894ab7d2fb18bddf12f215c8974ce
ATOROYO <davidatoroyosika@gmail.com>
2025-01-16 17:58:04 +0300

    Updsated the settings

--- my_organization_website/settings.py
+++ my_organization_website/settings.py
@@ -107,8 +107,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'davidatoroyosika@gmail.com' # Replace with your email
-EMAIL_HOST_PASSWORD = 'moni@TOO1' # Replace with your email password or app password
+EMAIL_HOST_USER = 'davidatoroyosika@gmail.com'  # Replace with your email
+EMAIL_HOST_PASSWORD = 'sharonlove'  # Replace with your email password
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
