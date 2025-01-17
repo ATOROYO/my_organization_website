8910a0db5d1934a06114e15fef8b1a1a73658bea
ATOROYO <davidatoroyosika@gmail.com>
2025-01-17 16:23:57 +0300

    Updated the models.py by adding ContactMessage model

--- main/models.py
+++ main/models.py
@@ -16,10 +16,11 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     
-class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return f"Message from {self.name}"
