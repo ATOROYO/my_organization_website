15833fadd316cc5e4d194c5ff2734c95da2f31d0
ATOROYO <davidatoroyosika@gmail.com>
2025-01-17 17:47:00 +0300

    Updated admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import ContactMessage
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(ContactMessage)
+admin.site.register(CustomUser, UserAdmin)
