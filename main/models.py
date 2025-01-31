a21f5aeeaa537af06c9dac602bbf26b5e5f0fc2c
ATOROYO <davidatoroyosika@gmail.com>
2025-01-31 09:41:31 +0300

    Updated main/models.py

--- main/models.py
+++ main/models.py
@@ -1,34 +1,87 @@
-from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+
+# class TeamMember(models.Model):
+#     name = models.CharField(max_length=100)
+#     position = models.CharField(max_length=100)
+#     bio = models.TextField()
+#     image = models.ImageField(upload_to='team/')
+
+# class GalleryImage(models.Model):
+#     title = models.CharField(max_length=100)
+#     image = models.ImageField(upload_to='gallery/')
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+    
+# class ContactMessage(models.Model):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField()
+#     message = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"Message from {self.name}"
+
+# class CustomUser(AbstractUser):
+#     phone_number = models.CharField(max_length=15, blank=True, null=True)
+#     address = models.TextField(blank=True, null=True)
+#     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+#     def __str__(self):
+#         return self.username
+
 from django.db import models
+from django.urls import reverse
+
+class Service(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='services/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('service_detail', args=[self.slug])
 
 class TeamMember(models.Model):
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
     bio = models.TextField()
-    image = models.ImageField(upload_to='team/')
+    photo = models.ImageField(upload_to='team/')
+    email = models.EmailField()
+    linkedin = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    display_order = models.PositiveIntegerField(default=0)
 
-class GalleryImage(models.Model):
-    title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='gallery/')
+    class Meta:
+        ordering = ['display_order']
 
-class Post(models.Model):
+    def __str__(self):
+        return f"{self.name} - {self.position}"
+
+class GalleryItem(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='gallery/')
+    date_taken = models.DateField()
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    is_resolved = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Message from {self.name}"
-
-class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-
-    def __str__(self):
-        return self.username
+        return f"Message from {self.name} - {self.created_at}"
