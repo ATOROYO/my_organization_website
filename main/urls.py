9054f504d274d090d9c207875b86bf7685118703
ATOROYO <davidatoroyosika@gmail.com>
2025-01-31 10:03:46 +0300

    Updated main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,29 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('about/', views.about, name='about'),
+#     path('services/', views.services, name='services'),
+#     path('team/', views.team, name='team'),
+#     path('gallery/', views.gallery, name='gallery'),
+#     path('contact/', views.contact, name='contact'),
+#     path('search/', views.search_results, name='search_results'),
+#     path('register/', views.register, name='register'),
+#     path('login/', views.login_view, name='login'),
+# ]
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('services/', views.services, name='services'),
-    path('team/', views.team, name='team'),
-    path('gallery/', views.gallery, name='gallery'),
-    path('contact/', views.contact, name='contact'),
-    path('search/', views.search_results, name='search_results'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    # path('search/', views.search, name='search'),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('users/', include('users.urls')),
+    path('services/', include('services.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
