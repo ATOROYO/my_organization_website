ccc501d4fbf564a3a5d891d420138e67f1ef5559
ATOROYO <davidatoroyosika@gmail.com>
2025-01-31 10:27:46 +0300

    Updated the settings and installed binary

--- my_organization_website/settings.py
+++ my_organization_website/settings.py
@@ -32,7 +32,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'my_organization_website.urls'
 
 TEMPLATES = [
     {
@@ -51,7 +51,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'my_organization_website.wsgi.application'
 
 DATABASES = {
     'default': {
